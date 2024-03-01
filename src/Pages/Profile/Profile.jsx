36775807b51631e9c57f0703ef6d71c9ebfb239c
Ahmed175 <https://github.com/Ahmed-175/chat-app-ahemd.git>
2024-03-01 14:47:59 -0800

    finish

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -14,45 +14,45 @@ function Profile() {
       <div className="projects">
         <h2>موقع للاختبارات</h2>
         <img src={tests}  className='img-project' />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aliquam iusto quas possimus laudantium est.</p>
+        <p>من اوائل المشاريع التي قمت صنعها هو عبارة عن موقع لستطيع كلم ن له حساب مشرف أن يضيف اختبارات و الطالاب الباقي يستطيعون حل هذه الامتحانات</p>
         <a href="https://tests-for-everyone-challenge.onrender.com" target='_black' className="btn-project">عرض الموقع</a>
       </div>
      
 
       <div className="projects">
-        <h2>Lorem ipsum dolor sit amet.</h2>
+        <h2>مشروع المخبوزات</h2>
         <img src={preview}  className='img-project' />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aliquam iusto quas possimus laudantium est.</p>
-                <a href="https://tests-for-everyone-challenge.onrender.com" target='_black' className="btn-project">عرض الموقع</a>
+        <p>هذا التصميم لست من صممه و لكن كان علي اليوتيوب لذلك درسته جيدا و استخلصت كل المعلومات التي فيه</p>
+                <a href="https://github.com/Ahmed-175/responsive-bakery-" target='_black' className="btn-project">عرض اكواد الموقع</a>
 
       </div>
       <div className="projects">
-        <h2>Lorem ipsum dolor sit amet.</h2>
+        <h2>الموقع الشخصي القديم</h2>
         <img src={old}  className='img-project' />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aliquam iusto quas possimus laudantium est.</p>
-        <a href="https://tests-for-everyone-challenge.onrender.com" target='_black' className="btn-project">عرض الموقع</a>
+        <p>هذا اول موقع شخصي قمت بصناعته في اعرض المشاريع الخاص بي</p>
+        <a href="https://github.com/Ahmed-175/old-profile" target='_black' className="btn-project">عرض أكواد الموقع</a>
 
       </div>
 
       <div className="projects">
-        <h2>Lorem ipsum dolor sit amet.</h2>
+        <h2>لعبة الشطرنج</h2>
         <img src={chess}  className='img-project' />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aliquam iusto quas possimus laudantium est.</p>
-                <a href="https://tests-for-everyone-challenge.onrender.com" target='_black' className="btn-project">عرض الموقع</a>
+        <p>من التي التي قمت بصنعها لعبة الشطرنج لم اكتمل منها بعد و لكن سوف اكملها لانها شوف تتطور عندي طريق تفكيري بشكل جيد</p>
+                <a href="https://github.com/Ahmed-175/chess-game" target='_black' className="btn-project">عرض أكواد الموقع</a>
 
       </div>
       <div className="projects">
-        <h2>Lorem ipsum dolor sit amet.</h2>
+        <h2>موقعي الجديد</h2>
         <img src={newpro}  className='img-project' />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aliquam iusto quas possimus laudantium est.</p>
-                <a href="https://tests-for-everyone-challenge.onrender.com" target='_black' className="btn-project">عرض الموقع</a>
+        <p>من الاشياء الجميله التي قمت بصناعتها لأنه دربنيعلي كيفية التغذية البصرية و  كيفية تنفيذ التصميمات</p>
+                <a href="/" target='_black' className="btn-project">عرض الموقع</a>
 
       </div>
       <div className="projects">
-        <h2>Lorem ipsum dolor sit amet.</h2>
+        <h2> جامعة العلوم بالاسكندرية</h2>
         <img src={uni}  className='img-project' />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aliquam iusto quas possimus laudantium est.</p>
-                <a href="https://tests-for-everyone-challenge.onrender.com" target='_black' className="btn-project">عرض الموقع</a>
+        <p>لقد طلبه مني احد في هذه الجامعة الموقع فكرته عرض مسارات و الاقسام الخاصة بي الجامعة و لكنه مازال في مرحلة البرمجة ولم ينهي بعد</p>
+              <a href="https://github.com/Ahmed-175/University-of-Science" target='_black' className="btn-project">عرض أكواد الموقع</a>
 
       </div>
 
